afb02b7c4d19a79cc32f9b72599271f6cfe9686b
Jakob Sinclair <sinclair.jakob@mailbox.org>
2018-08-07 17:25:41 +0200

    Add comments to player_piece.gd
    Signed-off-by: Jakob Sinclair <sinclair.jakob@mailbox.org>

--- player/player_piece.gd
+++ player/player_piece.gd
@@ -1,11 +1,11 @@
 extends KinematicBody
 
-const GRAVITY = 9.8;
-const GRAVITY_DIR = Vector3(0, -1, 0);
+const GRAVITY = 9.8; # Acceleration of gravity
+const GRAVITY_DIR = Vector3(0, -1, 0); # Direction of gravity
 
 var player_id = 0;
-var gravity = 0;
-var space = 1;
+var gravity = 0; # Accumulated speed
+var space = 1; # Space on the board the player is on
 var cookies = 0;
 var cakes = 0;
 
