5a67d699115bc8ce4ed322acd4a166507f109b52
Florian Kothmeier <floriankothmeier@web.de>
2018-08-12 10:00:03 +0000

    Add controller remapping and saving custom controls

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,5 +1,6 @@
 extends Container
 
+# path to the board being used on game start
 var board = ""
 var current_player = 1
 
@@ -9,7 +10,10 @@ var plugin_system = PluginSystem.new()
 var BoardLoader = preload("res://boards/boardloader.gd")
 var board_loader = BoardLoader.new()
 
-func _ready():
+var ControlRemapper = preload("res://menu/controlremapper.gd")
+var control_remapper = ControlRemapper.new(self)
+
+func load_boards():
 	var selection_board_list = get_node("Selection board/ScrollContainer/Buttons")
 	for board in board_loader.get_loaded_boards():
 		var button_template = preload("res://menu/board_selection_button.tscn")
@@ -17,6 +21,10 @@ func _ready():
 		board_list_entry.set_text(board)
 		board_list_entry.connect("pressed", self, "_on_board_select", [board_list_entry])
 		selection_board_list.add_child(board_list_entry)
+
+func _ready():
+	load_boards()
+	control_remapper.controls_remapping_setup()
 
 func _on_Play_pressed():
 	$"Main menu".hide()
@@ -49,6 +57,9 @@ func _on_board_select(target):
 
 func _on_Fullscreen_toggled(button_pressed):
 	OS.window_fullscreen = button_pressed
+
+func _input(event):
+	control_remapper._input(event)
 
 func _on_Selection_Char_Back_pressed():
 	$"Selection board".show()
