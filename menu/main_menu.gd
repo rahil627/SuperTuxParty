ad2a3a0183dfd1ce7765c8581d04e47fa000b1e1
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2018-08-29 01:42:01 -0300

    Various changes to the "menu" directory's code and scene organization

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,23 +1,22 @@
 extends Container
 
-# path to the board being used on game start
+# Path to the board being used on game start
 var board = ""
 var current_player = 1
 
-# path to the characters used on game start
+# Path to the characters used on game start
 var characters = []
 
 var board_loader
 var character_loader
 
-var ControlRemapper = preload("res://menu/controlremapper.gd")
-var control_remapper = ControlRemapper.new(self)
+var control_remapper = preload("res://menu/control_remapper.gd").new(self)
 
 func load_boards():
-	board_loader = get_node("/root/Global").board_loader
-	character_loader = get_node("/root/Global").character_loader
+	board_loader = Global.board_loader
+	character_loader = Global.character_loader
 	
-	var selection_board_list = get_node("Selection board/ScrollContainer/Buttons")
+	var selection_board_list = $SelectionBoard/ScrollContainer/Buttons
 	
 	for board in board_loader.get_loaded_boards():
 		var button_template = preload("res://menu/board_selection_button.tscn")
@@ -29,9 +28,9 @@ func load_boards():
 		selection_board_list.add_child(board_list_entry)
 
 func load_characters():
-	var selection_char_list = get_node("Selection char/Buttons/VScrollBar/Grid")
+	var selection_char_list = $SelectionChar/Buttons/VScrollBar/Grid
+	var button_template = preload("res://menu/character_selection_button.tscn")
 	for character in character_loader.get_loaded_characters():
-		var button_template = preload("res://menu/character_selection_button.tscn")
 		var character_list_entry = button_template.instance()
 		
 		var image = ResourceLoader.load(character_loader.get_character_splash(character)).get_data()
@@ -44,41 +43,41 @@ func load_characters():
 		selection_char_list.add_child(character_list_entry)
 
 func _ready():
-	$"Selection board/AwardType".add_item("Linear", 0);
-	$"Selection board/AwardType".add_item("Winner takes all", 1);
+	var award_type = $SelectionBoard/AwardType
+	award_type.add_item("Linear", Global.AWARD_T.linear);
+	award_type.add_item("Winner takes all", Global.AWARD_T.winner_only);
 	load_boards()
 	load_characters()
 	control_remapper.controls_remapping_setup()
-	characters.resize($"/root/Global".amount_of_players)
+	characters.resize(Global.amount_of_players)
 
 func _on_Play_pressed():
-	$"Main menu".hide()
-	$"Selection board".show()
+	$MainMenu.hide()
+	$SelectionBoard.show()
 
 func _on_Options_pressed():
-	$"Main menu".hide()
-	$"Options menu".show()
+	$MainMenu.hide()
+	$OptionsMenu.show()
 
 func _on_Quit_pressed():
 	get_tree().quit()
 
 func _on_Options_Back_pressed():
-	$"Main menu".show()
-	$"Options menu".hide()
+	$MainMenu.show()
+	$OptionsMenu.hide()
 
 func _on_Selection_Back_pressed():
-	$"Main menu".show()
-	$"Selection board".hide()
+	$MainMenu.show()
+	$SelectionBoard.hide()
 
 func _on_board_select(target):
-	$"/root/Global".new_game = true
+	Global.new_game = true
 	board = board_loader.get_board_path(target.get_text())
-	$"Selection board".hide()
-	$"Selection char".show()
-	$"PlayerInfo1".show()
-	$"PlayerInfo2".show()
-	$"PlayerInfo3".show()
-	$"PlayerInfo4".show()
+	$SelectionBoard.hide()
+	$SelectionChar.show()
+	
+	for i in range(1, 4):
+		get_node("PlayerInfo" + var2str(i)).show()
 
 func _on_Fullscreen_toggled(button_pressed):
 	OS.window_fullscreen = button_pressed
@@ -86,42 +85,39 @@ func _on_Fullscreen_toggled(button_pressed):
 func _input(event):
 	control_remapper._input(event)
 
-func _on_Selection_Char_Back_pressed():
-	$"Selection board".show()
-	$"Selection char".hide()
+func _on_SelectionChar_Back_pressed():
+	$SelectionBoard.show()
+	$SelectionChar.hide()
 	
 	current_player = 1
 	
-	$"Selection char/Title".text = "Select character for Player 1"
+	$SelectionChar/Title.text = "Select character for Player 1"
 	
 	for i in range(1, 4):
-		get_node("PlayerInfo" + var2str(i)).get_node("Character").text = "Character:"
-		get_node("PlayerInfo" + var2str(i)).get_node("Ready").text = "Not ready..."
-	
-	$"PlayerInfo1".hide()
-	$"PlayerInfo2".hide()
-	$"PlayerInfo3".hide()
-	$"PlayerInfo4".hide()
+		get_node("PlayerInfo" + var2str(i) + "/Character").text = "Character:"
+		get_node("PlayerInfo" + var2str(i) + "/Ready").text = "Not ready..."
+		get_node("PlayerInfo" + var2str(i)).hide()
 
 func _on_character_select(target):
-	get_node("PlayerInfo" + var2str(current_player)).get_node("Character").text = "Character: " + target.get_text()
-	get_node("PlayerInfo" + var2str(current_player)).get_node("Ready").text = "Ready!"
+	get_node("PlayerInfo" + var2str(current_player) + "/Character").text = "Character: " + target.get_text()
+	get_node("PlayerInfo" + var2str(current_player) + "/Ready").text = "Ready!"
 	
 	characters[current_player - 1] = target.get_text()
 	current_player += 1
 	
-	if current_player > $"/root/Global".amount_of_players:
+	if current_player > Global.amount_of_players:
 		var names = []
 		
 		for i in range(1, current_player):
-			names.push_back(get_node("PlayerInfo" + var2str(i)).get_node("Name").text)
+			names.push_back(get_node("PlayerInfo" + var2str(i) + "/Name").text)
 		
-		$"/root/Global".load_board(board, names, characters)
+		Global.load_board(board, names, characters)
 	
-	$"Selection char/Title".text = "Select character for Player " + var2str(current_player)
+	$SelectionChar/Title.text = "Select character for Player " + var2str(current_player)
 
 func _on_AwardType_item_selected(ID):
-	if ID == 0: # Linear
-		$"/root/Global".award = $"/root/Global".AWARD_T.linear
-	elif ID == 1: # Winner takes all
-		$"/root/Global".award = $"/root/Global".AWARD_T.winner_only
+	match ID:
+		Global.AWARD_T.linear:
+			Global.award = ID
+		Global.AWARD_T.winner_only:
+			Global.award = ID
