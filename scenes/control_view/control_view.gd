fc849cc872894e2f9c0815c66dbf387a48625556
florian <floriankothmeier@web.de>
2018-12-12 15:28:06 +0100

    Fix wrong paths in control_view.gd
    This caused joystick buttons and LEFT/UP/RIGHT/DOWN on the keyboard to not getting displayed if such an input should be displayed
    
    Signed-off-by: florian <floriankothmeier@web.de>

--- scenes/control_view/control_view.gd
+++ scenes/control_view/control_view.gd
@@ -6,25 +6,25 @@ func _ready():
 func display_key(event):
 	match event.scancode:
 		KEY_UP:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/up.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/up.png")
 		KEY_LEFT:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/left.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/left.png")
 		KEY_DOWN:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/down.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/down.png")
 		KEY_RIGHT:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/right.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/right.png")
 		_:
 			$Viewport/Label.text = OS.get_scancode_string(event.scancode)
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/key_background.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/key_background.png")
 
 func display_mouse_button(event):
 	match event.button_index:
 		BUTTON_LEFT:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/left_mouse.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/left_mouse.png")
 		BUTTON_RIGHT:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/right_mouse.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/right_mouse.png")
 		BUTTON_MIDDLE:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/middle_mouse.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/middle_mouse.png")
 		_:
 			$Viewport/Label.text = "Mouse " + var2str(event.button_index)
 
@@ -42,59 +42,59 @@ func display_joypad_button(event):
 		JOY_BUTTON_0:
 			match Global.joypad_display:
 				Global.JOYPAD_DISPLAY_TYPE.NUMBERS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/button3.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/button3.png")
 				Global.JOYPAD_DISPLAY_TYPE.XBOX:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonA.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonA.png")
 				Global.JOYPAD_DISPLAY_TYPE.NINTENDO_DS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonB.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonB.png")
 				Global.JOYPAD_DISPLAY_TYPE.PLAYSTATION:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonX.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonX.png")
 		JOY_BUTTON_1:
 			match Global.joypad_display:
 				Global.JOYPAD_DISPLAY_TYPE.NUMBERS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/button2.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/button2.png")
 				Global.JOYPAD_DISPLAY_TYPE.XBOX:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonB.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonB.png")
 				Global.JOYPAD_DISPLAY_TYPE.NINTENDO_DS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonA.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonA.png")
 				Global.JOYPAD_DISPLAY_TYPE.PLAYSTATION:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonCircle.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonCircle.png")
 		JOY_BUTTON_2:
 			match Global.joypad_display:
 				Global.JOYPAD_DISPLAY_TYPE.NUMBERS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/button4.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/button4.png")
 				Global.JOYPAD_DISPLAY_TYPE.XBOX:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonX.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonX.png")
 				Global.JOYPAD_DISPLAY_TYPE.NINTENDO_DS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonY.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonY.png")
 				Global.JOYPAD_DISPLAY_TYPE.PLAYSTATION:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonSquare.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonSquare.png")
 		JOY_BUTTON_3:
 			match Global.joypad_display:
 				Global.JOYPAD_DISPLAY_TYPE.NUMBERS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/button3.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/button3.png")
 				Global.JOYPAD_DISPLAY_TYPE.XBOX:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonY.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonY.png")
 				Global.JOYPAD_DISPLAY_TYPE.NINTENDO_DS:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonX.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonX.png")
 				Global.JOYPAD_DISPLAY_TYPE.PLAYSTATION:
-					$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonTriangle.png")
+					$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonTriangle.png")
 		JOY_SELECT:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonSelect.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonSelect.png")
 		JOY_START:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonStart.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonStart.png")
 		JOY_L2:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonL.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonL.png")
 		JOY_L:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonL1.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonL1.png")
 		JOY_L3:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonL2.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonL2.png")
 		JOY_R2:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonR.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonR.png")
 		JOY_R:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonR1.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonR1.png")
 		JOY_R3:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonR2.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonR2.png")
 		_:
 			$Viewport/Label.text = "Joypad " + var2str(event.button_index)
 
@@ -102,28 +102,28 @@ func display_joypad_axis(event):
 	match event.axis:
 		JOY_ANALOG_LX:
 			if event.axis_value < 0:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowLeft.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowLeft.png")
 			else:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowRight.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowRight.png")
 		JOY_ANALOG_LY:
 			if event.axis_value < 0:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowUp.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowUp.png")
 			else:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowDown.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowDown.png")
 		JOY_ANALOG_RX:
 			if event.axis_value < 0:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowLeft.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowLeft.png")
 			else:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowRight.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowRight.png")
 		JOY_ANALOG_RY:
 			if event.axis_value < 0:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowUp.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowUp.png")
 			else:
-				$Viewport/TextureRect.texture = load("res://assets/control_view/images/arrowDown.png")
+				$Viewport/TextureRect.texture = load("res://assets/textures/controls/arrowDown.png")
 		JOY_ANALOG_L2:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonL.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonL.png")
 		JOY_ANALOG_R2:
-			$Viewport/TextureRect.texture = load("res://assets/control_view/images/buttonR.png")
+			$Viewport/TextureRect.texture = load("res://assets/textures/controls/buttonR.png")
 		_:
 			if event.axis_value < 0:
 				$Viewport/Label.text = "Axis -" + var2str(event.axis)
