83de98aa2ad66fd8ea8dc6df39883766b21bfa81
Florian Kothmeier <floriankothmeier@web.de>
2019-08-03 12:15:53 +0200

    Fix crash in hurdle minigame

--- scripts/utility.gd
+++ scripts/utility.gd
@@ -1,15 +1,15 @@
 extends Node
 
-func apply_nextpass_material(material: Material, node: Spatial) -> void:
+func apply_nextpass_material(material: Material, node: Node) -> void:
 	if node is MeshInstance:
-		var mat: Material = node.get_surface_material(0)
+		var mat: Material = (node as MeshInstance).get_surface_material(0)
 		var i := 0
 		while mat != null:
 			while mat.next_pass != null:
 				mat = mat.next_pass
 			mat.next_pass = material
 			i += 1
-			mat = node.get_surface_material(i)
+			mat = (node as MeshInstance).get_surface_material(i)
 
 	for child in node.get_children():
 		apply_nextpass_material(material, child)
