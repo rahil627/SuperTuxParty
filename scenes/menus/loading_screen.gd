88a478b853fa72c7f7bbdb7a0e86b564ff760daf
Florian Kothmeier <floriankothmeier@web.de>
2020-09-26 02:36:09 +0200

    Fix error reported by Godot when min = max for progressbar in loading screen
    Signed-off-by: Florian Kothmeier <floriankothmeier@web.de>

--- scenes/menus/loading_screen.gd
+++ scenes/menus/loading_screen.gd
@@ -2,6 +2,11 @@ extends Control
 
 func _process(_delta):
 	var progress = Global.get_loader_progress()
+	
+	if progress[1] == 0:
+		progress[1] = 1
+		progress[0] = 1
+	
 	$CenterContainer/VBoxContainer/ProgressBar.max_value = progress[1]
 	$CenterContainer/VBoxContainer/ProgressBar.value = progress[0]
 	
